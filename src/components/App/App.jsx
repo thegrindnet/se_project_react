dff2b2f00d702e908d6615f51a99c24589c5b354
David Bonilla <thegrindnet@gmail.com>
2025-09-13 02:27:19 -0600

    Add open and close functionality to modals

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,28 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
 
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
       </div>
-      <ModalWithForm buttonText="Add garment" title="New garment">
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+        handleCloseClick={closeActiveModal}
+      >
         <label htmlFor="name" className="modal__label">
           Name{" "}
           <input
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,11 +1,23 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
-    <div className="modal">
+    <div
+      className={`modal ${activeModal === "add-garment" && "modal__opened"}`}
+    >
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button className="modal__close-btn" type="button"></button>
+        <button
+          onClick={handleCloseClick}
+          className="modal__close-btn"
+          type="button"
+        ></button>
         <form className="modal__form">
           {children}
           <button className="modal__submit" type="submit">
